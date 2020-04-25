09b0a2a6c585970af953d22d806443b5f8de091d
Dávid Gurzó <dgurzo@gmail.com>
2020-04-25 21:21:38 +0200

    new classes & interfaces

---
+++ IEpulet.java
@@ -0,0 +1,4 @@
+
+public interface IEpulet extends ITargy {
+	
+}
---
+++ Jegesmedve.java
@@ -0,0 +1,40 @@
+
+public class Jegesmedve implements IKarakter {
+
+	@Override
+	public void lep(int irany) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hovihar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void elsut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tesoTeVizbeEstel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void huzdKi(Szereplo sz) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hitByMedve() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
