753621d96ff384954645cbf7da35b0fc56e6fdd7
Vago Benedek <w7bxkk@gmail.com>
2020-04-26 13:19:00 +0200

    Finishing init sequence

--- MezoFactory.java
+++ MezoFactory.java
@@ -1,6 +1,7 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MezoFactory {
 	public MezoFactory() throws IOException {
@@ -8,7 +9,39 @@ public class MezoFactory {
 		f.append("MezoFactory letrejott\n");
 		f.close();
 	}
-	public ArrayList<Mezo> createMezo(int i) {
-		return new ArrayList<Mezo>();
+	public List<Mezo> createMezo(int i) throws IOException {
+		List<Mezo> mezok = new ArrayList<>();
+		int row = (int) Math.sqrt(i);
+		for (int j = 0; j != i ; j++){
+			mezok.add(new Mezo());
+
+		}
+		for (int j = 0; j != i; j++){
+			if (j%row == 0){
+				mezok.get(j).setSzomszedMezo(new Tenger(), 3);
+			}
+			else {
+				mezok.get(j).setSzomszedMezo(mezok.get(j+1), 3);
+			}
+			if (j < row){
+				mezok.get(j).setSzomszedMezo(new Tenger(), 0);
+			}
+			else {
+				mezok.get(j).setSzomszedMezo(mezok.get(j+row), 0);
+			}
+			if (Math.floor(j/row) == row-1){
+				mezok.get(j).setSzomszedMezo(new Tenger(), 1);
+			}
+			else {
+				mezok.get(j).setSzomszedMezo(mezok.get(j-row), 1);
+			}
+			if (j%row == row-1){
+				mezok.get(j).setSzomszedMezo(new Tenger(), 2);
+			}
+			else {
+				mezok.get(j).setSzomszedMezo(mezok.get(j-1), 2);
+			}
+		}
+		return mezok;
 	}
 }
