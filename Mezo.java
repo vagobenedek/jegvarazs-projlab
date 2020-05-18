c7cd26849f67d1ebbc015487a4572715099aea59
Gémesi Szabolcs <gemesiszabolcs99@gmail.com>
2020-05-18 17:48:25 +0200

    targy felvetel es torekeny lapat

--- Mezo.java
+++ Mezo.java
@@ -410,8 +410,7 @@ abstract public  class Mezo {
 	 * @throws IOException
 	 */
 	public void targyFelvetele(Szereplo sz) throws IOException {
-		//0425
-		if(this.isFeltort() && targy != null) {
+		if(this.isFeltort() && targy != null&&this.getHoSzint()<=0) {
 			targy.felvesz(sz);
 		}
 		else {
